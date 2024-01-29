4bad9b7cf79d59828791b0dfb7b21adbc7456b01
nitrobacck <milos.hd.djokic@gmail.com>
2024-01-29 19:44:10 +0100

    Hello, World!

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <strstream>
+#include <string>
+
+int main(const int ac, const char* argv[]) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
